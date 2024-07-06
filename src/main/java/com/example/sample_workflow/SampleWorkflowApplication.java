67bde65b2cd26d0367e0993da1d8b5c3c37f897a
sajithpk87 <sajithpkasthuri@gmail.com>
2024-07-06 12:12:01 +0530

    Add changes

---
+++ src/main/java/com/example/sample_workflow/SampleWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample_workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleWorkflowApplication.class, args);
+	}
+
+}
